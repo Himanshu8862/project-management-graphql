b9d8a58a2365bf331fc9b7f1d2cc7dfa61c10c12
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-09-17 13:36:15 +0530

    single project page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./components/Header"
-import Clients from "./components/Clients"
-import AddClientModal from "./components/AddClientModal"
-import Projects from "./components/Projects"
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import Project from "./pages/Project"
 
 
 // to resolve the existing and incoming warning when an item is deleted
@@ -35,12 +36,16 @@ function App() {
     return (
         <>
             <ApolloProvider client={client}>
-                <Header />
-                <div className="container">
-                    <AddClientModal />
-                    <Projects/>
-                    <Clients />
-                </div>
+                <Router>
+                    <Header />
+                    <div className="container">
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/projects/:id" element={<Project />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Routes>
+                    </div>
+                </Router>
             </ApolloProvider>
         </>
     )
